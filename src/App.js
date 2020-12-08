9879032a8f909a4e56c0a5b274114da4699e4433
Talha Uzun <talhauzun600@gmail.com>
2020-12-08 17:57:48 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import AddTodo from './components/addTodo'
+import TodoList from './components/todoList'
+import VisibilityFilter from './components/visibilityFilter'
+import {Provider} from 'react-redux'
+import store from './redux/store'
 
 function App() {
   return (
+    <Provider store={store}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TODO Managers</h1>
+      <AddTodo/>
+      <TodoList/>
+      <VisibilityFilter/>
     </div>
+    </Provider>
   );
 }
 
---
+++ src/redux/store.js
@@ -0,0 +1,6 @@
+
+import {createStore, combineReducers} from 'redux'
+import {todos, visibilityFilter} from './reducers'
+
+export default createStore(combineReducers({todos, visibilityFilter}), 
+window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
